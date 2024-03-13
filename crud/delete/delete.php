e48dffa4de6dc70d416cd4381b2e1918a4f79e0a
Ren <monkey.d.luffy.r1130@gmail.com>
2024-03-14 00:53:59 +0900

    詳細と削除を完成

--- crud/delete/delete.php
+++ crud/delete/delete.php
@@ -19,34 +19,31 @@ if (isset($_SESSION['staff_login']) == false) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>投稿完了</title>
+    <title>投稿削除</title>
 </head>
 <body>
-    <h1>以下の内容で投稿が完了しました。</h1>
+    <h1>以下の内容を削除しますか？</h1>
     <?php
     try {
         require_once('../../common/security.php');
         require_once('../../common/db_config.php');
         
-        $date = date('Y/m/d', strtotime($_POST['date']));
-        $title = $_POST['title'];
-        $body = $_POST['body'];
+        $id = (int)$_GET['id'];
 
-        $sql = 'INSERT INTO comment_keep (date,title,body) VALUES (?,?,?)';
+        $sql = 'SELECT * FROM comment_keep WHERE id = ?';
         $stmt = $dbh->prepare($sql);
-        $stmt->bindValue(1, $date, PDO::PARAM_STR);
-        $stmt->bindValue(2, $title, PDO::PARAM_STR);
-        $stmt->bindValue(3, $body, PDO::PARAM_STR);
+        $stmt->bindValue(1, $id, PDO::PARAM_INT);
         $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
         $dbh = null;
 
         echo '<h2>投稿日</h2>';
-        echo $date . '<br>';
+        echo $result['date'] . '<br>';
         echo '<br>';
         echo '<h2>タイトル</h2>';
-        echo $title . '<br>';
+        echo $result['title'] . '<br>';
         echo '<h2>本文</h2>';
-        echo $body . '<br>';
+        echo $result['body'] . '<br>';
 
     } catch (PDOException $e) {
         echo 'エラー発生: ' . htmlspecialchars($e->getMessage(), ENT_QUOTES);
@@ -54,6 +51,10 @@ if (isset($_SESSION['staff_login']) == false) {
     }
     ?>
     <br>
-    <a href="../../staff_info.php">お知らせ一覧に戻る</a>
+    <form method="post" action="delete_check.php?id=<?= $result['id']; ?>">
+        <input type="submit" value="はい">
+        <input type="button" onclick="history.back()" value="いいえ">
+    </form>
+    
 </body>
 </html>
