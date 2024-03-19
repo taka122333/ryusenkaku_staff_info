f1eec4c1ee888e1b32d69ae67bfb20520078658b
高橋力也 <131872192+taka122333@users.noreply.github.com>
2024-03-19 16:07:22 +0900

    Update db_config.php
    セキュリティ関連の変更

--- common/db_config.php
+++ common/db_config.php
@@ -9,6 +9,6 @@ $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 //サーバー用
 // $user = 'xs144235_wp1';
-// $pass = 'jq6p7ce4sy';
+// $pass = 'ここにパスワードを入れる';
 // $dbh = new PDO('mysql:host=localhost;dbname=xs144235_wp1;charset=utf8', $user, $pass);
 // $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
